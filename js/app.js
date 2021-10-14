8d421f176dacd0db7a82990cc1b3b63cc455b3f1
Siddhesh Patil <sidpatil1770@gmail.com>
2021-10-14 22:53:32 +0530

    Cowin card generator done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 // scanVaccineByPin returns bool if it found vaccine
-
 function scanVaccineByPin(pincode, date) {       //date = dd-mm-yyyy
     let url = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pincode}&date=${date}`
     let r = new XMLHttpRequest();
@@ -20,4 +19,61 @@ function scanVaccineByPin(pincode, date) {       //date = dd-mm-yyyy
         }
     }
 }
+function setupCowinCard(i) {
+    // let mainArea = document.getElementById('mainArea');
+    // let elem = document.createElement('div');
 
+    document.getElementById(`card${i}`).innerHTML = `<div class="cowinh3">
+    <h3>Cowin Tracker</h3><br>
+<div class="pincode" id="pincode${i}">
+    <label for="">Enter pincode - </label>
+    <input type="number" id="pincode1" class="pincodeAccept" maxlength="6">
+</div><br>
+<div class="date1">
+    <label>Select Date - </label>
+    <input type="date" id="cowinDate${i}">
+</div><br>
+<div class="searchBtnCowin">
+    <button class="newBtn" id="cowinTrackBtn${i}">Track</button>
+
+</div>
+<br><br></div>`;
+    // document.querySelector(`div#cowinCard${i}`).appendChild(elem);
+}
+function createNewCard() {
+    if (localStorage.getItem('cardSerial') === null) {//new
+        localStorage.setItem('cardSerial', '0')//new
+    }//new
+    let elem = document.createElement('div');
+    elem.setAttribute('class', 'cardLayout');
+    let i = parseInt(localStorage.getItem('cardSerial'));
+    elem.setAttribute(`id`, `card${i}`);      // new
+    document.querySelector('div.playground').appendChild(elem);
+    // new
+
+    if (document.getElementById('userCardOption').value === 'cowinTracker') {
+        setupCowinCard(i);
+    }// add other invokes in elif
+    i = i + 1;
+    localStorage.setItem('cardSerial', i)//new
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -------------Event Listeners----------------------------------------
+document.getElementById('newCardBtn').addEventListener('click', function () {
+    let btnId = this.id;
+    createNewCard(btnId);
+});
